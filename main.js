503a74c187c6c36abb2ce89025a26f1dbf40d87a
Prajeeth Emanuel <daedra.emanuel.779@gmail.com>
2021-11-19 21:00:06 -0500

    node changes

--- main.js
+++ main.js
@@ -4,22 +4,39 @@ const {
 
 const url = 'https://goo.gl/MV8Tkh'
 
-extract(url).then((article) => {
-  console.log(article.content)
-}).catch((err) => {
-  console.log(err)
-})
+
 
 const express = require('express')
 const app = express()
 const port = 3000
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/website/:page', (req, res) => {
-  res.send(req.params)
+
+	console.log(decodeURIComponent(req.params.page))
+
+	var decodedURL = decodeURIComponent(req.params.page)
+	extract(decodedURL).then((article) => {
+	  res.send(article.content)
+	}).catch((err) => {
+	  console.log(err)
+	})
+
+
+
+	// res.send(req.params)
 })
 
 app.listen(port, () => {
