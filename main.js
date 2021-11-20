a7731f61c08e8857fb4c5d1b805c11a48e93cd6e
Prajeeth Emanuel <daedra.emanuel.779@gmail.com>
2021-11-19 20:38:29 -0500

    adding code

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ extract(url).then((article) => {
 }).catch((err) => {
   console.log(err)
 })
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/website/:page', (req, res) => {
+  res.send(req.params)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
